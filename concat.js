bff79b5acf3c7b29b28591cdcce0d66262051c94
necipbaser <necipbaser71@gmail.com>
2023-03-26 16:08:33 +0300

    last commit

--- concat.js
+++ concat.js
@@ -1,8 +1,12 @@
 
 const arr1 = ['A','B','C']
-
 const arr2 = ['D','E','F']
-
 const newArr = arr1.concat(arr2)
-
 console.log(newArr);
+
+const newArr2 = arr1.concat(arr2,'G');
+console.log(newArr2)
+
+const numbers = [[1,2],[3,4]]
+console.log(numbers.concat(5));
+console.log(numbers.concat([[5,6]]))
